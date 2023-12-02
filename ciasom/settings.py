d6ee5752ce79493b1c254279617ba42f864b34db
MegaZizon <105596059+MegaZizon@users.noreply.github.com>
2023-12-03 08:23:07 +0900

    Update settings.py

--- ciasom/settings.py
+++ ciasom/settings.py
@@ -21,12 +21,13 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = "django-insecure-z4(%wl*tf0%s_n4l@)%g-q5!w^wmt$e8qq6-oq0rdvbiltgy4j"
-
+SECRET_KEY = os.environ.get('SECRET_KEY','django-insecure-z4(%wl*tf0%s_n4l@)%g-q5!w^wmt$e8qq6-oq0rdvbiltgy4j')
+DEBUG = int(os.environ.get('DEBUG',1))
+if os.environ.get('DJANGO_ALLOWED_HOSTS'):
+    ALLOWED_HOSTS = os.environ.get('DJANGO_ALLOWED_HOSTS').split(' ')
+else:
+    ALLOWED_HOSTS = ["*"]
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
-
-ALLOWED_HOSTS = ["*"]
 EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST='smtp.gmail.com'
 EMAIL_USE_TLS=True
@@ -84,9 +85,13 @@ WSGI_APPLICATION = "ciasom.wsgi.application"
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
 
 DATABASES = {
-    "default": {
-        "ENGINE": "django.db.backends.sqlite3",
-        "NAME": BASE_DIR / "db.sqlite3",
+    'default': {
+        'ENGINE': os.environ.get('SQL_ENGINE','django.db.backends.sqlite3'),
+        'NAME': os.environ.get('SQL_DATABASE', os.path.join(BASE_DIR,'db.sqlite3')),
+        'USER': os.environ.get('SQL_USER','user'),
+        'PASSWORD': os.environ.get('SQL_PASSWORD','password'),
+        'HOST': os.environ.get('SQL_HOST','localhost'),
+        'PORT': os.environ.get('SQL_PORT','5432'),
     }
 }
 
@@ -126,6 +131,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
 STATIC_URL = "static/"
+STATIC_ROOT =  os.path.join(BASE_DIR,'staticfiles')
 STATICFILES_DIRS = [
     BASE_DIR / "static",
 ]
